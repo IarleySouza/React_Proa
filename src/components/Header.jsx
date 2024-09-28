ca0043584850606af085a4e087dc7ff137beb894
Iarley Souza <167232053+IarleySouza@users.noreply.github.com>
2024-09-27 21:18:25 -0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+function Header() {
+    return(
+        <>
+            <div className="inicio">
+                <h1>
+                    Header
+                </h1>
+            </div>
+        </>
+    )
+}
+export default Header
